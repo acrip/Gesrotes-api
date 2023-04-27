f78e3d687c1d5aabff98c7b3bbf2bc831db0378b
acrip <jazapata@unicauca.edu.co>
2023-04-27 02:29:07 -0500

    Firs commit

---
+++ src/main/java/co/edu/unicauca/gesrotes/GesrotesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.gesrotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesrotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesrotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicauca/gesrotes/GesrotesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.gesrotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesrotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
